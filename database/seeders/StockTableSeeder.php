155c395ca823129657408fbe935bd20165d332a9
Armash <armashash@gmail.com>
2021-02-25 12:51:59 +0500

    seeder for stock table added

---
+++ database/seeders/StockTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Stock;
+use Illuminate\Database\Seeder;
+
+class StockTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $stockItems = [
+            'Retail',
+            'Return',
+            'Repair'
+        ];
+
+        foreach ($stockItems as $items)
+        {
+            Stock::create(['name' => $items]);
+        }
+    }
+}
