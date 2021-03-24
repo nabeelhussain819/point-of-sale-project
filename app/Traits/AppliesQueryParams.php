6a2a7e05adf5d9f152fb2f7ae65a2891fdf5a5bb
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-03-24 05:44:28 +0500

    Update applied query params

--- app/Traits/AppliesQueryParams.php
+++ app/Traits/AppliesQueryParams.php
@@ -8,11 +8,13 @@
 
 namespace App\Traits;
 
+
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 
 trait AppliesQueryParams
 {
-    public function applyFilters(&$request): callable
+    public function applyFilters(Request &$request): callable
     {
         return function (Builder $query) use (&$request) {
             $query->when($request->get('id'), function (Builder $query, $id) {
