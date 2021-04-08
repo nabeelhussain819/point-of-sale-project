36db933c69ac8476ae2bb22313b6bcd8a4cdf48e
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-04-09 03:23:22 +0500

    minor modeule update

--- resources/views/admin/repair/index.blade.php
+++ resources/views/admin/repair/index.blade.php
@@ -6,7 +6,7 @@
 
     <div class="container bg-white">
         <div class="row" style="align-items: center">
-            <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="col-lg-3 col-md-3 col-sm-3">
                 <a href="{{route('deviceType')}}" style="">
                     <div class="card shadow-lg bg-white rounded"
                          style="height: 120px; background-image: linear-gradient(87deg, #3c3f72  0%, #8187ec  100%) !important; border-radius: .375rem; ">
@@ -23,7 +23,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="col-lg-3 col-md-3 col-sm-3">
                 <a href="{{route('brand')}}" style="color: #fff;">
                     <div class="card shadow-lg bg-white rounded"
                          style="height: 120px; background-image: linear-gradient(87deg, #ef3232  0%, #ff6666  100%) !important; border-radius: .375rem;">
@@ -40,7 +40,7 @@
                     </div>
                 </a>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="col-lg-3 col-md-3 col-sm-3">
                 <a href="{{route('issue-type')}}" style="color: #fff;">
                     <div class="card shadow-lg bg-white rounded"
                          style="height: 120px; background-image: linear-gradient(87deg, #6da252 0%, #b8d3a9 100%); border-radius: .375rem;">
@@ -51,6 +51,23 @@
                                 </div>
                                 <div class="col">
                                     <h5 class="mt-0 text-light">Issue Type</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3">
+                <a href="{{route('associate-device-brand')}}" style="color: #fff;">
+                    <div class="card shadow-lg bg-white rounded"
+                         style="height: 120px; background-image: linear-gradient(87deg, #2980B9 0%, #6DD5FA 100%); border-radius: .375rem;">
+                        <div class="card-body text-center">
+                            <div class="row">
+                                <div class="col">
+                                    <i class="fas fa-cubes text-light" style="font-size: 70px"></i>
+                                </div>
+                                <div class="col">
+                                    <h5 class="mt-0 text-light">Associate Types</h5>
                                 </div>
                             </div>
                         </div>
