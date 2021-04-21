dc5f768199d048241d456a6c08f31f4bebe25555
Muhammad naveed nasib <naveednasib@hotmail.com>
2021-04-21 06:12:54 +0500

    remove payable static

--- app/Observers/FinanceObserver.php
+++ app/Observers/FinanceObserver.php
@@ -11,7 +11,7 @@ class FinanceObserver
     public function creating(Finance $finance)
     {
         $finance->store_id = Store::currentId();
-        $finance->payable = 1;//temp
+
         if (!empty($finance->start_date)) {
             $finance->end_date = $finance->start_date->addMonths($finance->duration_period);
         }
