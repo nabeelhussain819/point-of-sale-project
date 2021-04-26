e912f7fa24cde9cd532998ad50d27cbc4001bf59
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-04-27 03:39:57 +0500

    minor change

--- resources/js/module/product/add.vue
+++ resources/js/module/product/add.vue
@@ -1,87 +1,98 @@
 <template>
-  <a-row :gutter="16">
-    <a-col :span="11">
-      <a-form-item
-        placeholder="insert id and press enter"
-        :validate-status="fetchProductsErrors.validateStatus"
-        :help="fetchProductsErrors.errorMsg"
-        label="Scan Product Number"
-      >
-        <a-input
-          @pressEnter="getProductById"
-          placeholder="Insert search key"
-          v-decorator="[
-            'product_id',
-            {
-              rules: rules,
-            },
-          ]"
-        /> </a-form-item
-    ></a-col>
-    <!-- <a-col :span="2"> <a-form-item :colon="false" label="OR">|</a-form-item></a-col>
+    <a-row :gutter="16">
+        <a-col :span="11">
+            <a-form-item
+                placeholder="insert id and press enter"
+                :validate-status="fetchProductsErrors.validateStatus"
+                :help="fetchProductsErrors.errorMsg"
+                label="Scan Product Number"
+            >
+                <a-input-search
+                    @pressEnter="getProductById"
+                    placeholder="Insert search key"
+                    v-decorator="[
+                        'product_id',
+                        {
+                            rules: rules
+                        }
+                    ]"
+                    @search="getProductById"
+                >
+                    <a-button type="primary" slot="enterButton">
+                        <a-icon type="mobile" />
+                    </a-button>
+                </a-input-search> </a-form-item
+        ></a-col>
+        <!-- <a-col :span="2"> <a-form-item :colon="false" label="OR">|</a-form-item></a-col>
     <a-col :span="11">
       <a-form-item label="Add Item">
         <a-button type="primary">Add Item </a-button>
       </a-form-item>
     </a-col> -->
-  </a-row>
+    </a-row>
 </template>
 <script>
 import InventoryService from "../../services/API/InventoryService";
 import { isEmpty } from "../../services/helpers";
 import { EVENT_CUSTOMERSALE_PRODUCT_ADD } from "../../services/constants";
 export default {
-  props: {
-    rules: {
-      default: () => [],
+    props: {
+        rules: {
+            default: () => []
+        }
     },
-  },
-  data() {
-    return {
-      fetchProductsErrors: {},
-      customer: null,
-    };
-  },
-  methods: {
-    getProductById(e) {
-      this.resetValidation();
-      let productId = e.target.value;
-      InventoryService.products({ product_id: productId, OrUPC: productId })
-        .then((inventory) => {
-          this.noProductFound(inventory);
-          if (!isEmpty(inventory)) {
-            this.$eventBus.$emit(EVENT_CUSTOMERSALE_PRODUCT_ADD, inventory.product);
-          }
-        })
-        .catch((ex) => {
-          if (ex.response.status === 409) {
-            this.fetchProductsErrors = {
-              validateStatus: "error",
-              errorMsg: ex.response.data.message,
-            };
-          }
-          if (ex.response.status === 404) {
-            this.fetchProductsErrors = {
-              validateStatus: "error",
-              errorMsg: "No Product Found Against Key",
-            };
-          }
-        });
-    },
-    noProductFound(data) {
-      if (isEmpty(data)) {
-        this.fetchProductsErrors = {
-          validateStatus: "error",
-          errorMsg: "No Product Found Against Key",
+    data() {
+        return {
+            fetchProductsErrors: {},
+            customer: null
         };
-      }
     },
-    resetValidation() {
-      this.fetchProductsErrors = {};
+    methods: {
+        getProductById(e) {
+            this.resetValidation();
+            let productId = isEmpty(e.target) ? e : e.target.value;
+            InventoryService.products({
+                product_id: productId,
+                OrUPC: productId
+            })
+                .then(inventory => {
+                    this.noProductFound(inventory);
+                    if (!isEmpty(inventory)) {
+                        this.$eventBus.$emit(
+                            EVENT_CUSTOMERSALE_PRODUCT_ADD,
+                            inventory.product
+                        );
+                    }
+                })
+                .catch(ex => {
+                    if (ex.response.status === 409) {
+                        this.fetchProductsErrors = {
+                            validateStatus: "error",
+                            errorMsg: ex.response.data.message
+                        };
+                    }
+                    if (ex.response.status === 404) {
+                        this.fetchProductsErrors = {
+                            validateStatus: "error",
+                            errorMsg: "No Product Found Against Key"
+                        };
+                    }
+                });
+        },
+        noProductFound(data) {
+            if (isEmpty(data)) {
+                this.fetchProductsErrors = {
+                    validateStatus: "error",
+                    errorMsg: "No Product Found Against Key"
+                };
+            }
+        },
+        resetValidation() {
+            this.fetchProductsErrors = {};
+        }
     },
-  },
-  beforeDestroy() {
-    // window.removeEventListener(EVENT_CUSTOMERSALE_PRODUCT_ADD, "asd");
-  },
+    beforeDestroy() {
+        // window.removeEventListener(EVENT_CUSTOMERSALE_PRODUCT_ADD, "asd");
+    }
 };
 </script>
--- resources/js/module/sales/quick-sale.vue
+++ resources/js/module/sales/quick-sale.vue
@@ -1,50 +1,61 @@
 <template>
-  <div>
-    <a-card title="Quick Sale">
-      <a-form @submit="handleSubmit">
-        <a-form-item></a-form-item>
+    <div>
+        <a-card title="Quick Sale">
+            <a-form @submit="handleSubmit">
+                <a-form-item></a-form-item>
 
-        <AddProduct :rules="rules" />
-        <a-button :disabled="disabled" htmlType="submit" type="primary"> Sale </a-button>
-      </a-form>
-    </a-card>
-  </div>
+                <AddProduct :rules="rules" />
+                <!-- <a-button :disabled="disabled" htmlType="submit" type="primary"> Sale </a-button> -->
+            </a-form>
+        </a-card>
+    </div>
 </template>
 <script>
 import AddProduct from "../product/add";
 import { EVENT_CUSTOMERSALE_PRODUCT_ADD } from "../../services/constants";
+import { isEmpty } from "../../services/helpers";
 export default {
-  data() {
-    return {
-      disabled: true,
-      formLayout: "horizontal",
-      form: this.$form.createForm(this, { name: "addRepair" }),
-      rules: [{ required: true, message: "Please input your customer number!" }],
-      product: {},
-    };
-  },
-  components: {
-    AddProduct,
-  },
-  mounted() {
-    let setProducts = this.updatedProducts;
-    this.$eventBus.$on(EVENT_CUSTOMERSALE_PRODUCT_ADD, function (product) {
-      setProducts(product);
-    });
-  },
-  methods: {
-    updatedProducts(product) {
-      this.disabled = false;
-      this.product = product;
+    data() {
+        return {
+            disabled: true,
+            formLayout: "horizontal",
+            form: this.$form.createForm(this, { name: "addRepair" }),
+            rules: [
+                {
+                    required: true,
+                    message: "Please input your customer number!"
+                }
+            ],
+            product: {}
+        };
     },
-    handleSubmit(e) {
-      e.preventDefault();
-      this.form.validateFields((err, values) => {
-        if (!err) {
-          window.location.href = `/sales/create?OrUPC=${this.product.id}`;
+    components: {
+        AddProduct
+    },
+    mounted() {
+        let setProducts = this.updatedProducts;
+        this.$eventBus.$on(EVENT_CUSTOMERSALE_PRODUCT_ADD, function(product) {
+            setProducts(product);
+            if (!isEmpty(product.serial_number)) {
+                return (window.location.href = `/sales/create?OrUPC=${product.serial_number}`);
+            }
+
+            return (window.location.href = `/sales/create?OrUPC=${product.id}`);
+        });
+    },
+    methods: {
+        updatedProducts(product) {
+            this.disabled = false;
+            this.product = product;
+        },
+        handleSubmit(e) {
+            e.preventDefault();
+            this.form.validateFields((err, values) => {
+                if (!err) {
+                    window.location.href = `/sales/create?OrUPC=${this.product.id}`;
+                }
+            });
         }
-      });
-    },
-  },
+    }
 };
 </script>
