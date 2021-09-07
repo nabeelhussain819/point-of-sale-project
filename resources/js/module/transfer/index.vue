0a8107349a060a946b9e9eb130595f9bacf4edcd
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-09-08 03:28:16 +0500

    transfer stocker fiels

---
+++ resources/js/module/transfer/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <span>
+        <list />
+    </span>
+</template>
+
+<script>
+import list from "./list";
+export default {
+    components: { list },
+    data() {
+        return {};
+    }
+};
+</script>
