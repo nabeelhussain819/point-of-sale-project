73b291ff3c699c7405434e7924a98812c35be917
Muhammad naveed nasib <naveed.nasib@hotmail.com>
2021-10-18 01:12:22 +0500

    nvneotry zerop handlng

--- app/Observers/InventoryObserver.php
+++ app/Observers/InventoryObserver.php
@@ -13,7 +13,7 @@ class InventoryObserver
         if (!$inventory->OUTGOING_PRODUCTS) {
             $inventory->quantity = $inventory->getOriginal('quantity') + $inventory->quantity;
         }
-        if ($inventory < 0) {
+        if ($inventory->quantity < 0) {
             throw new Exception("inventory has been less than zero form that operation please check the case ");
         }
     }
