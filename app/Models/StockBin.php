a35c69425b5120a3037cd7914dde0e804c325e2a
nasib <mnnasib7777@gmail.com>
2022-05-10 09:34:32 +0500

    return to vendor table added

--- app/Models/StockBin.php
+++ app/Models/StockBin.php
@@ -14,8 +14,8 @@ use Illuminate\Database\Eloquent\Model;
 class StockBin extends Model
 {
 
-    const RETAIL=1;
-    const TYPE_RETURN=2;
+    const RETAIL = 1;
+    const TYPE_RETURN = 2;
     /**
      * The "type" of the auto-incrementing ID.
      *
@@ -25,8 +25,9 @@ class StockBin extends Model
 
     /**
      * @var array
+     * @!!! @constant is language variable will cause an issue
      */
-    protected $fillable = ['name','constant','created_at', 'updated_at'];
+    protected $fillable = ['name', 'constant', 'created_at', 'updated_at', 'system'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
