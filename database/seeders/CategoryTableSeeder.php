c294f4869c808e0698a9fe907fecadfbf8834c34
nabeelhussain819 <hnabeel570@gmail.com>
2022-05-20 08:41:44 -1200

    task complete

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Category; 
+use App\Models\Category;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
--- resources/js/mixins/pagination.js
+++ resources/js/mixins/pagination.js
@@ -1,32 +1,31 @@
 export default {
-    data() {
-        return {
-            pagination: {
-                // current: 1,
-                pageSize: 10,
-                // total: 0,
-                // showTotal: () => `Total ${this.pagination.total}`,
-                onChange: (current, pageSize) =>
-                    this.pageSelect(current, pageSize)
-            }
-        };
+  data() {
+    return {
+      pagination: {
+        // current: 1,
+        pageSize: 10,
+        // total: 0,
+        // showTotal: () => `Total ${this.pagination.total}`,
+        onChange: (current, pageSize) => this.pageSelect(current, pageSize),
+      },
+    };
+  },
+  methods: {
+    setPagination(data) {
+      const pagination = { ...this.pagination };
+      pagination.current = data.current_page;
+      pagination.page = data.current_page;
+      pagination.pageSize = data.per_page;
+      pagination.total = data.total;
+      this.pagination = pagination;
     },
-    methods: {
-        setPagination(data) {
-            const pagination = { ...this.pagination };
-            pagination.current = data.current_page;
-            pagination.page = data.current_page;
-            pagination.pageSize = data.per_page;
-            pagination.total = data.total;
-            this.pagination = pagination;
-        },
-        pageSelect(current, pageSize) {
-            const pagination = { ...this.pagination };
-            pagination.current = current;
-            pagination.page = current;
-            pagination.pageSize = pageSize;
-            this.pagination = pagination;
-            this.fetch();
-        }
-    }
+    pageSelect(current, pageSize) {
+      const pagination = { ...this.pagination };
+      pagination.current = current;
+      pagination.page = current;
+      pagination.pageSize = pageSize;
+      this.pagination = pagination;
+      this.fetch();
+    },
+  },
 };
